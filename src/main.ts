c85538c616cf1a783a8da129655b4533d9c85261
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2025-03-21 20:56:09 +0700

    fix: typeorm

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { setupSwagger } from 'swagger';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
