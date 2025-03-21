d83af416fb9344b283de82402e43c42e1231d8eb
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2025-03-21 20:45:06 +0700

    fix: typeorm

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  console.log('Environment Variables:', process.env);
   const app = await NestFactory.create(AppModule);
 
   // enable shutdown hooks explicitly.
