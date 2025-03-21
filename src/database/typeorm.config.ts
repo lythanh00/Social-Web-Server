640c29ca813bcb3e2dc9d9adc983cc4165b5d7c2
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2025-03-21 18:41:36 +0700

    fix: typeorm

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -25,9 +25,7 @@ export const typeOrmConfigAsync = {
       database: configService.get<string>('MYSQL_DATABASE'),
     };
 
-    console.log({
-      MYSQLHOST: process.env.MYSQLHOST,
-    });
+    console.log('MYSQLHOST:', process.env.MYSQLHOST);
 
     console.log('Database Config:', databaseConfig); // Log thông tin cấu hình
 
