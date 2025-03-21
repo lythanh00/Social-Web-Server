8c39b97e42f3f29a2e64cb9a4e8c876465acfd71
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2025-03-21 11:56:45 +0700

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,11 @@ async function bootstrap() {
   app.enableCors();
   setupSwagger(app);
   //app.useLogger();
-  await app.listen(3000);
+  // await app.listen(3000);
+
+  // Lắng nghe trên cổng từ biến môi trường hoặc mặc định là 3000
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`Application is running on: http://localhost:${PORT}`);
 }
 bootstrap();
