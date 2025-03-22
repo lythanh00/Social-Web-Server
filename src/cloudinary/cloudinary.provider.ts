78468e206831b5e6debe6bd114a9ade197327370
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2025-03-22 12:20:54 +0700

    fix: cloudinary

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -1,12 +1,29 @@
 import { v2 } from 'cloudinary';
 import { CLOUDINARY } from './constants';
+
 export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: () => {
-    return v2.config({
-      cloud_name: process.env.CLD_CLOUD_NAME,
-      api_key: process.env.CLD_API_KEY,
-      api_secret: process.env.CLD_API_SECRET,
-    });
+    try {
+      const configResult = v2.config({
+        cloud_name: process.env.CLD_CLOUD_NAME,
+        api_key: process.env.CLD_API_KEY,
+        api_secret: process.env.CLD_API_SECRET,
+      });
+
+      // Log thông tin để xác nhận kết nối
+      if (configResult) {
+        console.log(
+          'Cloudinary connection successfully configured:',
+          configResult.cloud_name,
+        );
+      } else {
+        console.error('Cloudinary configuration failed');
+      }
+
+      return configResult;
+    } catch (error) {
+      console.error('Error connecting to Cloudinary:', error.message);
+    }
   },
 };
